cb8fe1d730c060621c81f71229dc4dc8e813c458
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-04-28 19:14:41 +0530

    Added function to (partially) simulate compression stages
    Doesn't simulate for work required or exergy

--- src/compressor.c
+++ src/compressor.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 const double max_pressure_ratio = 1.2;
 
@@ -43,4 +45,34 @@ int find_num_stages(double desired_rp)
 	}
 
 	return n;
+}
+
+struct compressor *simulate_compressor(
+	int num_stages,
+	double p_atm,
+	double t_inlet,
+	double gamma)
+{
+	struct compressor *cs = malloc (num_stages * sizeof(*cs));
+
+	// input pressure and temperature from previous compressor.
+	// initially from atmosphere
+	double p_prev = p_atm;
+	double t_prev = t_inlet;
+
+	for (int i = 0; i < num_stages; i++) {
+		cs[i].p_in = p_prev;
+		cs[i].t_in = t_prev;
+
+		cs[i].p_out = cs[i].p_in * 1.2;
+
+		// assuming that compressor is adiabatic
+		cs[i].t_out = pow (cs[i].p_in / cs[i].p_out, 1 - gamma)
+			* cs[i].t_in;
+
+		p_prev = cs[i].p_out;
+		t_prev = cs[i].t_out;
+	}
+
+	return cs;
 }
