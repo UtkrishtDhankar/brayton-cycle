92a364ccd6b3414bf6caf7c3e31cf436d7dd598c
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-04-30 10:15:11 +0530

    Added simulate_boiler()
    Also changed boiler struct to have req_flow rate.

--- src/boiler.c
+++ src/boiler.c
@@ -7,6 +7,8 @@ struct boiler {
 
 	double t_in;
 	double t_out;
+
+	double req_flow_rate;
 };
 
 double calc_fuel_flow_rate(
@@ -18,3 +20,19 @@ double calc_fuel_flow_rate(
 	return gas_flow_rate * (c_air / c_fuel) * (t_out - t_in) / efficiency;
 }
 
+struct boiler simulate_boiler(
+	double t_in,		// desired outlet temperature of the gas in K
+	double p,
+	double t_out,		// inlet temperature of the gas in K
+	double gas_flow_rate	// flow rate of the gas in kg/s
+) {
+	struct boiler b;
+
+	b.p = p;
+	b.t_in = t_in;
+	b.t_out = t_out;
+
+	b.req_flow_rate = calc_fuel_flow_rate(t_in, t_out, gas_flow_rate);
+
+	return b;
+}
