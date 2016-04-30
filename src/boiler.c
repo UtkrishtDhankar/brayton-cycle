a2756b3c1bf8c6683f7b1ccb10fa8541fc86c934
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-04-30 10:10:41 +0530

    Added function to calculate required flow rate

--- src/boiler.c
+++ src/boiler.c
@@ -1,3 +1,7 @@
+const double efficiency = 0.98;	// efficiency of combustion
+const double c_fuel = 4200000;	// calorific value of fuel in J/kg K.
+const double c_air = 1005;	// specific heat of air at const p in J/kg K.
+
 struct boiler {
 	double p_in;
 	double p_out;
@@ -5,3 +9,13 @@ struct boiler {
 	double t_in;
 	double t_out;
 };
+
+double calc_fuel_flow_rate(
+	double t_in,		// desired outlet temperature of the gas in K
+	double t_out,		// inlet temperature of the gas in K
+	double gas_flow_rate	// flow rate of the gas in kg/s
+)
+{
+	return gas_flow_rate * (c_air / c_fuel) * (t_out - t_in) / efficiency;
+}
+
