7ea6b3c07d4360ee191acb5fae7a57247fc024a4
DeathStocker <DeathStocker@users.noreply.github.com>
2016-05-06 16:32:28 +0530

    Added pressure ratio at individual stage.
    Computed pressure ratio at individual stage and calculated t_out, and p_out with it.

--- src/turbine.c
+++ src/turbine.c
@@ -18,10 +18,18 @@ void print_turbine(struct turbine turb)
 }
 
 /*
+ * Finds pressure ratio of 1 stage
+ */
+double press_rat(double p_comp)
+{
+	return pow(p_final / p_comp, 1 / num_stages);
+}
+
+/*
  * Simulates the turbine for given inputs.
  */
 struct turbine *simulate_turbine(
-	double p_comp,           // the inlet pressure/output from compressor, in Pa.
+	double p_comp,          // the inlet pressure/output from compressor, in Pa.
 	double t_inlet,         // the inlet temp, in K.
 	double gamma,           // the gamma of the gas.
 	double gas_flow_rate,   // the rate at which gas flows in, in kg/s.
@@ -35,6 +43,7 @@ struct turbine *simulate_turbine(
 	// input temperature from the compressor.
 	double p_prev = p_comp;
 	double t_prev = t_inlet;
+	double max_pressure_ratio = press_rat(p_comp);
 
 	for (int i = 0; i < num_stages; i++) {
 		tb[i].p_in = p_prev;
@@ -43,7 +52,7 @@ struct turbine *simulate_turbine(
 		tb[i].p_out = tb[i].p_in * max_pressure_ratio;
 
 		// assuming that turbine is adiabatic
-		tb[i].t_out = pow(1 / max_pressure_ratio, 1 - gamma)
+		tb[i].t_out = pow(1 / max_pressure_ratio, (1 - gamma) / gamma)
 			      * tb[i].t_in;
 
 		p_prev = tb[i].p_out;
@@ -52,7 +61,7 @@ struct turbine *simulate_turbine(
 		// Assuming turbine work is integral Vdp, with ineficiencies.
 		tb[i].w_req = (1 / stage_efficiency)
 			      * (gas_flow_rate / molecular_mass)
-			      * r_univ * (tb[i].t_out - tb[i].t_in) / (gamma - 1);
+			      * r_univ * (tb[i].t_out - tb[i].t_in) * gamma / (gamma - 1);
 
 		tb[i].exergy_gain = stage_efficiency * tb[i].w_req;
 		tb[i].exergy_loss = tb[i].w_req - tb[i].exergy_gain;
--- src/turbine.h
+++ src/turbine.h
@@ -6,10 +6,11 @@
 #include <math.h>
 
 const double max_pressure_ratio = 1.2;
-const double r_univ = 8.314;            // in SI units.
+const double r_univ = 8.314;                                    // in SI units.
 const int num_stages = 8;
-const double eta_isen = 0.92;           //Isentropic efficiency
-const double eta_comb = 0.98;           //Efficiency of combustion
+const double eta_isen = 0.92;                                   //Isentropic efficiency
+const double eta_comb = 0.98;                                   //Efficiency of combustion
+const double p_final = 108000;                                  //Final pressure.
 
 struct turbine {
 	double p_in;            // the pressure flowing in, in Pascals.
@@ -37,7 +38,7 @@ void print_turbine(struct turbine c);
  * Simulates the turbine for given inputs.
  */
 struct turbine *simulate_turbine(
-	double p_comp,           // the inlet pressure/outlet pressure from compressor, in Pa.
+	double p_comp,          // the inlet pressure/outlet pressure from compressor, in Pa.
 	double t_inlet,         // the inlet temp, in K.
 	double gamma,           // the gamma of the gas.
 	double gas_flow_rate,   // the rate at which gas flows in, in kg/s.
