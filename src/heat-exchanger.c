8e2d6e702301954e8b48be178aea18aa70f1d5fb
DeathStocker <DeathStocker@users.noreply.github.com>
2016-05-08 19:00:36 +0530

    Fixes #20
    Changed comments and added documentation before the functions.

--- src/heat-exchanger.c
+++ src/heat-exchanger.c
@@ -1,19 +1,27 @@
 #include "heat-exchanger.h"
 
+/*
+   *Calculates the required flow rate for the gas
+ */
 double calc_fuel_flow_rate(
-	double t_in,		// desired outlet temperature of the gas in K
-	double t_out,		// inlet temperature of the gas in K
-	double gas_flow_rate	// flow rate of the gas in kg/s
-) {
+	double t_in,            // Temperature of gas entering from compressor in K.
+	double t_out,           // Temperature of gas leaving the heat exchanger in K.
+	double gas_flow_rate    // Flow rate of the gas in kg/s
+	)
+{
 	return gas_flow_rate * (c_air / c_fuel) * (t_out - t_in) / efficiency;
 }
 
+/*
+   *Simulates the heat-exchanger for the given inputs.
+ */
 struct boiler simulate_boiler(
-	double t_in,		// desired outlet temperature of the gas in K
-	double p,		// pressure at which to use boiler
-	double t_out,		// inlet temperature of the gas in K
-	double gas_flow_rate	// flow rate of the gas in kg/s
-) {
+	double t_in,            // Temperature of gas entering from compressor in K.
+	double p,               // Pressure at which to use the heat exchanger.
+	double t_out,           // Temperature of gas leaving the heat exchanger in K.
+	double gas_flow_rate    // Flow rate of the gas in kg/s.
+	)
+{
 	struct boiler b;
 
 	b.p = p;
--- src/heat-exchanger.h
+++ src/heat-exchanger.h
@@ -1,30 +1,36 @@
 #ifndef HEAT_EXCHANGER_H
 #define HEAT_EXCHANGER_H
 
-const double efficiency = 0.98;	// efficiency of combustion
-const double c_fuel = 4200000;	// calorific value of fuel in J/kg K.
-const double c_air = 1005;	// specific heat of air at const p in J/kg K.
+const double efficiency = 0.98; // Efficiency of combustion
+const double c_fuel = 4200000;  // Calorific value of fuel in J/kg K.
+const double c_air = 1005;      // Specific heat of air at const p in J/kg K.
 
+/*
+   *Calculates the required flow rate for the gas
+ */
 struct boiler {
-	double p;
+	double p;               // Pressure of heat exchanger in Pa.
 
-	double t_in;
-	double t_out;
+	double t_in;            // Temperature of gas entering the heat exchanger in K.
+	double t_out;           // Temperature of gas leaving the heat exchanger in K.
 
-	double req_flow_rate;
+	double req_flow_rate;   //The required flow rate for the gas to flow through the heat exchanger in kg/s.
 };
 
 double calc_fuel_flow_rate(
-	double t_in,		// desired outlet temperature of the gas in K
-	double t_out,		// inlet temperature of the gas in K
-	double gas_flow_rate	// flow rate of the gas in kg/s
-);
+	double t_in,            // Temperature of gas entering the heat exchnager in K.
+	double t_out,           // Temperature of gas leaving the heat exchanger in K.
+	double gas_flow_rate    // Flow rate of the gas in kg/s
+	);
 
+/*
+   *Simulates the heat-exchanger for the given inputs.
+ */
 struct boiler simulate_boiler(
-	double t_in,		// desired outlet temperature of the gas in K
-	double p,		// pressure at which to use boiler
-	double t_out,		// inlet temperature of the gas in K
-	double gas_flow_rate	// flow rate of the gas in kg/s
-);
+	double t_in,            // Temperature of gas entering from compressor in K.
+	double p,               // Pressure at which to use the heat exchanger.
+	double t_out,           // Temperature of gas leaving the heat exchanger in K.
+	double gas_flow_rate    // Flow rate of the gas in kg/s.
+	);
 
 #endif
