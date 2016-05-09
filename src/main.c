b3e75347d781cbe5ad857cb9024b62d2ba828861
DeathStocker <DeathStocker@users.noreply.github.com>
2016-05-09 15:04:38 +0530

    Fixes #24

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include "compressor.h"
-#include "boiler.h"
+#include "combustion-chamber.h"
 #include "turbine.h"
 
 #include <stdlib.h>
@@ -14,29 +14,39 @@ int main()
 
 	int c_num_stages = find_num_stages(desired_rp);
 
+	double p_atm = 100000;
+	double comp_t_inlet = 273.15 + 15;
+	double gamma = 1.4;
+	double gas_flow_rate = 1
+	double molecular_mass = 0.025
+	double stage_efficiency = 0.92
+
 	struct compressor *c_stages =
 	simulate_compressor(
 		c_num_stages,
-		100000,
-		273 + 15,
-		1.4,
-		1,
-		0.025,
-		0.92);
+		p_atm,
+		comp_t_inlet,
+		gamma,
+		gas_flow_rate,
+		molecular_mass,
+		stage_efficiency);
 
-	struct boiler b = simulate_boiler(
+	struct combustion_chamber b = simulate_combustion_chamber(
 		c_stages[c_num_stages - 1].t_out,
 		c_stages[c_num_stages - 1].p_out,
 		turbine_t_in,
-		1);
+		gas_flow_rate);
+
+	double p_comp = b.p;
+	double t_inlet = b.t_out;
 
 	struct turbine *t_stages = simulate_turbine(
 		b.p,
 		b.t_out,
-		1.4,
-		1,
-		0.025,
-		0.92);
+		gamma,
+		gas_flow_rate,
+		molecular_mass,
+		stage_efficiency);
 
 	printf("\n\n---- The Compressor Stages ----\n");
 	for (int i = 0; i < c_num_stages; i++)
@@ -50,12 +60,13 @@ int main()
 	double w_c = 0;
 	double w_t = 0;
 	double heat_added;
+	double specific_heat = 4200000;
 
 	for (int i = 0; i < c_num_stages; i++)
 		w_c += c_stages[i].w_req;
 	for (int i = 0; i < 8; i++)
 		w_t += t_stages[i].w_out;
-	heat_added = 4200000 * b.req_flow_rate;
+	heat_added = specific_heat * b.req_flow_rate;
 
 	n = (w_t - w_c) / heat_added;
 
