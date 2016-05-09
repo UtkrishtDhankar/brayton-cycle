cb2f9303a7dda620b86b24634b134db72a025e9a
DeathStocker <DeathStocker@users.noreply.github.com>
2016-05-09 15:24:38 +0530

    Update boiler.c

--- src/boiler.c
+++ src/boiler.c
@@ -1,5 +1,9 @@
 #include "boiler.h"
 
+double efficiency = 0.98;	// efficiency of combustion
+double c_fuel = 4200000;	// calorific value of fuel in J/kg K.
+double c_air = 1005;	// specific heat of air at const p in J/kg K.
+
 double calc_fuel_flow_rate(
 	double t_in,		// desired outlet temperature of the gas in K
 	double t_out,		// inlet temperature of the gas in K
@@ -8,13 +12,13 @@ double calc_fuel_flow_rate(
 	return gas_flow_rate * (c_air / c_fuel) * (t_out - t_in) / efficiency;
 }
 
-struct combustion_chamber simulate_combustion_chamber(
+struct boiler simulate_boiler(
 	double t_in,		// desired outlet temperature of the gas in K
-	double p,		// pressure at which to use combustion chamber
+	double p,		// pressure at which to use boiler
 	double t_out,		// inlet temperature of the gas in K
 	double gas_flow_rate	// flow rate of the gas in kg/s
 ) {
-	struct combustion_chamber b;
+	struct boiler b;
 
 	b.p = p;
 	b.t_in = t_in;
